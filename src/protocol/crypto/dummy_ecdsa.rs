c3603a6603095b9bdc6ccb535a9d12c1ada83d50
qusuyan <qusuyan@gmail.com>
2024-05-20 21:01:33 -0500

    update encryption time

--- src/protocol/crypto/dummy_ecdsa.rs
+++ src/protocol/crypto/dummy_ecdsa.rs
@@ -12,8 +12,8 @@ pub fn gen_key_pair(seed: u64) -> (PubKey, PrivKey) {
 }
 
 pub async fn sign(_privkey: &PrivKey, input: &[u8]) -> Result<Signature, CopycatError> {
-    const SIGN_TIME: f64 = 0.241e-3; // measured with k256 for messages < 1KB
-    const HASH_TIME_PER_BYTE: f64 = 5.24e-9;
+    const SIGN_TIME: f64 = 0.0799e-3; // measured with k256 for messages ranging from 1B to 1MB
+    const HASH_TIME_PER_BYTE: f64 = 5.30e-9;
     let start = Instant::now();
     let sign_time = HASH_TIME_PER_BYTE * input.len() as f64 + SIGN_TIME;
     tokio::time::sleep_until(start + Duration::from_secs_f64(sign_time)).await;
@@ -25,8 +25,8 @@ pub async fn verify(
     input: &[u8],
     _signature: &Signature,
 ) -> Result<bool, CopycatError> {
-    const VERIFY_TIME: f64 = 0.4e-3; // measured with k256 for messages < 1KB
-    const HASH_TIME_PER_BYTE: f64 = 5.24e-9;
+    const VERIFY_TIME: f64 = 0.144e-3; // measured with k256 for messages ranging from 1B to 1MB
+    const HASH_TIME_PER_BYTE: f64 = 5.30e-9;
     let start = Instant::now();
     let verify_time = HASH_TIME_PER_BYTE * input.len() as f64 + VERIFY_TIME;
     tokio::time::sleep_until(start + Duration::from_secs_f64(verify_time)).await;
